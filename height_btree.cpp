d487a7527691bcd6dce6d8875f2746ec885eef4c
adarshkumar8225 <51961214+adarshkumar8225@users.noreply.github.com>
2020-06-06 20:53:41 +0530

    height of btree

---
+++ height_btree.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/int height=0,max=0;
+int getHeight(struct node* root) {
+    // Write your code here
+    
+    struct node *trav;
+    if(trav=NULL ) return;
+    
+    trav=root;
+    while((trav->left && trav->right)!=NULL){
+        height=height+1;
+        getHeight(trav->left);
+        getHeight(trav->right);
+    }
+    if(max<height) max=height;
+    height=0;
+
+   return max;
+    
+}
+
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  
+	printf("%d",getHeight(root));
+    return 0;
+}
